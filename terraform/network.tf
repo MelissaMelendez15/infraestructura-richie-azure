88c6cfb1e9428517dadb9e5eb7064de357f2c340
MelissaMelendez15 <MelissaMelendez15@gmail.com>
2025-07-12 16:18:43 +0200

    feat: se crea otra reglade seguridad para configurar el puerto 8080:80

--- terraform/network.tf
+++ terraform/network.tf
@@ -37,5 +37,44 @@ resource "azurerm_network_interface" "nic_richie" {
         private_ip_address_allocation = "Dynamic"
         public_ip_address_id = azurerm_public_ip.public_ip_richie.id
     }
-   
+}
+
+# Se crea el NSG con la regla de SSH(puerto 22)
+
+resource "azurerm_network_security_group" "nsg_richie" {
+    name = "nsg_richie"
+    location = var.location
+    resource_group_name = var.azurerm_group_name
+
+   # Regla de SSH(puerto 22)
+    security_rule {
+        name = "Allow-SSH"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+    
+    # Se agrega segunda regla para utilizar el puerto 8080:80
+    security_rule {
+        name = "Allow-SSH-8080"
+        priority = 1002
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "8080"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+# Se asocia el NSG a la Subred
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_richie" {
+    subnet_id = azurerm_subnet.subnet_richie.id
+    network_security_group_id = azurerm_network_security_group.nsg_richie.id
 }
